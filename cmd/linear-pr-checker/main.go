190b7b8a2c58f8d1adea28a684c39608369be7ae
Ikaika <kai@momenthouse.com>
2021-10-14 16:36:30 -0700

    output vars

--- cmd/linear-pr-checker/main.go
+++ cmd/linear-pr-checker/main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	fmt.Printf("Hello %s", os.Args[1])
 	fmt.Printf("GITHUB_REPOSITORY %s", os.Getenv("GITHUB_REPOSITORY"))
-	fmt.Printf("GITHUB_RUN_NUMBER  %s", os.Getenv("GITHUB_RUN_NUMBER"))
+	fmt.Printf("GITHUB_EVENT_PATH  %s", os.Getenv("GITHUB_EVENT_PATH"))
 	fmt.Printf("GITHUB_REPOSITORY_OWNER %s", os.Getenv("GITHUB_REPOSITORY_OWNER"))
 }
